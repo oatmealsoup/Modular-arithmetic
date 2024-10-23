100b7197686d5e5edf2f95768663344f61492388
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-23 18:24:26 -0400

    Update userInteraction.js

--- userInteraction.js
+++ userInteraction.js
@@ -1,5 +1,5 @@
 import { generateTable } from './tableGenerator.js';
-
+import { mapCellsToGrayScale } from '.greyScale.js';
 function handleZoom(event) {
   // Implement zooming logic here
 }
@@ -12,3 +12,13 @@ document.getElementById('limit').addEventListener('change', () => {
   const limit = parseInt(document.getElementById('limit').value);
   generateTable(limit);
 });
+
+function applyGrayscale() {
+  const cells = document.querySelectorAll('table td');
+  cells.forEach((cell) => {
+    const value = parseInt(cell.textContent);
+    const color = mapValueToGrayScale(value);
+    cell.style.backgroundColor = color;
+	cell.style.textColor = 'orange';
+  });
+}
