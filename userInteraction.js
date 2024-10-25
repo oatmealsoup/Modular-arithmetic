ca1963098309aeb9bbcdd35b6dcf48d589d1363a
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-24 23:38:50 -0400

    Update userInteraction.js

--- userInteraction.js
+++ userInteraction.js
@@ -1,13 +1,16 @@
 import { generateTable } from './tableGenerator.js';
 
+document.getElementById('limit').addEventListener('change', () => {
+  const limit = parseInt(document.getElementById('limit').value);
+  generateTable(limit);
+});
+
 function adjustPageSize() {
   const tableHeight = document.getElementById('tableContainer').offsetHeight;
   const tableWidth = document.getElementById('tableContainer').offsetWidth;
-
   document.body.style.minHeight = `${tableHeight}px`;
   document.body.style.minWidth = `${tableWidth}px`;
 }
-
-// Initial call to generate the table and adjust the page size
-generateTable(50); // Replace 10 with your desired initial limit
 adjustPageSize();
+
+
