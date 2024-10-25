fd7353e5a03cc0e91db0fcb65466285b52df01c5
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-24 23:50:32 -0400

    Update tableGenerator.js

--- tableGenerator.js
+++ tableGenerator.js
@@ -49,8 +49,7 @@ export function generateTable(limit) {
         else if (174 >= value >= 80) { 
         cell.style.color = '#ffffff'; // White text for 0
         cell.style.backgroundColor = `#${hexValue}${hexValue}${hexValue}`;
-      }
-      } else {
+      }else {
         const hexValue = Math.abs(value).toString(16).padStart(2, '0');
         const invertedHexValue = (255 - Math.abs(value)).toString(16).padStart(2, '0');
 
