d5d715d0032b19bd4f7add6a84fb780add3e74ee
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-26 03:12:53 -0400

    Update userInteraction.js

--- userInteraction.js
+++ userInteraction.js
@@ -3,10 +3,6 @@ import { generateTable } from './tableGenerator.js';
 document.getElementById('limit').addEventListener('change', () => {
   const limit = parseInt(document.getElementById('limit').value);
   generateTable(limit);
-  const tableHeight = document.getElementById('tableContainer').offsetHeight;
-  const tableWidth = document.getElementById('tableContainer').offsetWidth;
-  document.body.style.minHeight = `${tableHeight}px`;
-  document.body.style.minWidth = `${tableWidth}px`;
 });
 
 
