b398d3ceeb77967fab22050a6ea6b55ec0f88282
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-27 22:42:30 -0400

    Update tableGenerator.js

--- tableGenerator.js
+++ tableGenerator.js
@@ -11,39 +11,48 @@
     const colorValue = Math.round(normalized * 255);
     colorMap[i] = `rgb(${colorValue}, ${colorValue}, ${colorValue})`;
   }
-
+    
+  //Table construction
   for (let i = -limit; i <= limit; i++) {
     const row = document.createElement('tr');
 
     for (let j = -limit; j <= limit; j++) {
       const cell = document.createElement('td');
 
+      //Bold indices
       if (i === -limit || j === -limit) {
         cell.classList.add('bold-text');
       }
+      //Cell with "mod"
       if (i === -limit && j === -limit) {
         cell.textContent= "mod"
       }
+      //Index column
       if (i === -limit) {
         cell.textContent = j;
       }
+      //Index row
        if (j === -limit) {
         cell.textContent = i;
        }
+      //Modular calculations
        if(i > -limit && j > -limit) {
-          cell.textContent = i % j;
+        cell.textContent = i % j;
+        
+        //Absolute value for colors
+        const natural = Math.abs(cell.textContent);
 
-          if (j !== 0) {
-          const natural = Math.abs(cell.textContent);
+         //Apply colors to background
+        if (j !== 0) {
           cell.style.backgroundColor = colorMap[natural];
           }
-          if (natural > limit / 2) {
+           //Apply colors to text
+          if ( > limit / 2) {
             cell.classList.add('light-blue');
           } else {
             cell.classList.add('dark-blue');
           }
       }
-
       row.appendChild(cell);
     }
     fragment.appendChild(row);
