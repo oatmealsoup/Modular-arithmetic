502e75e1f84249c58a1b107d587c463f144edab2
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-29 15:59:18 -0400

    Update modularArithmetic.js

--- modularArithmetic.js
+++ modularArithmetic.js
@@ -1,15 +1,20 @@
 function mod() {
+//Clear storage
+  localStorage.removeItem('a');
+  localStorage.removeItem('b');
+  
+// Get a and b
   const a = parseInt(document.getElementById('a').value);
   const b = parseInt(document.getElementById('b').value);
-  localStorage.clear();
 
-  // Modulo calculation
+
+// Modulo calculation
   const result = (a-b)*Math.floor(a/b);
 
-  // Store values locally
+// Store values locally
   localStorage.setItem('a', a);
   localStorage.setItem('b', b);
 
-  // Display the result
+// Display the result
   document.getElementById('result').textContent = `The result of ${a} mod ${b} (for Euclidean division) is: ${result}`;
 }
