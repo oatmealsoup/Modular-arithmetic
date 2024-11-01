afc56599400dc10ab7db1b2900b3c0f57c1c83a0
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-11-01 07:59:23 -0400

    Update tableGenerator.js

--- tableGenerator.js
+++ tableGenerator.js
@@ -25,7 +25,7 @@ for (let i = 0; i <= 2 * limit + 1; i++) {
 cells[i] = [];
 	for (let j = 0; j <= 2 * limit + 1; j++) {
 		//Adjusting the indices in a variables makes properties and their calculations concise, introducing integers to the array.
-		//Again, the indices must be flipped to make the index row and column, requiring exchange of the modulo function's indice inputs for readability when the flip is implemented.
+		//Again, the indices must be flipped to make the index row and column, requiring exchange of the modulo function's indice inputs for readability and consistency.
 		const adjustedI = j - limit - 1; 
 		const adjustedJ = i - limit - 1;
 		
@@ -36,13 +36,12 @@ cells[i] = [];
 		cells[i][j] = {textContent: adjustedI, backgroundShade: '', color: '', className: "bold-text"};
 		} else if (j === 0) {
 		cells[i][j] = {textContent: adjustedJ, backgroundShade: '', color: '', className: "bold-text"};
-		} else if (j === limit + 1 && i !== 0 ) {
-		cells[i][j] = {textContent: NaN, backgroundShade: '', color: 'black', className: "bold-text"};
-		// Non-index cells use a function to calculate column modulo row, have a shade propery based on the absolute value, and use two colors for contrasting shades.
 		} else {
 		const modulo = adjustedJ - (adjustedI * Math.floor(adjustedJ / adjustedI));
+		if (modulo != NaN){
 		const natural = Math.abs(modulo);
 		cells[i][j] = {textContent: modulo, backgroundShade: shadeMap[natural], color: (natural > limit / 2 ? 'darkblue' : 'lightblue')};
+		}
 		}
 	}
 }
