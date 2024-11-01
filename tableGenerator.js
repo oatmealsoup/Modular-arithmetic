2de83c8a7b7d80d2987d7b1df0da07dc58a31935
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-11-01 06:58:20 -0400

    Update tableGenerator.js

--- tableGenerator.js
+++ tableGenerator.js
@@ -32,15 +32,15 @@ cells[i] = [];
 		
 		//Index cells are created for i === 0 || j === 0, with i === 0 && j === 0 containing the string "mod".
 		if (i === 0 && j === 0){
-		cells[i][j] = {textContent: "mod"};
+		cells[i][j] = {textContent: "mod"}
 		} else if (i === 0){
-		cells[i][j] = {textContent: adjustedI, backgroundShade: '', color: '', className: "bold-text"};
+		cells[i][j] = {textContent: adjustedI, backgroundShade: '', color: '', className: "bold-text"}
 		} else if (j === 0) {
-		cells[i][j] = {textContent: adjustedJ, backgroundShade: '', color: '', className: "bold-text"};
+		cells[i][j] = {textContent: adjustedJ, backgroundShade: '', color: '', className: "bold-text"}
 		} else if (j === limit + 2 %% i! === 0 ) {
-		cells[i][j] = {textContent: NaN, backgroundShade: '', color: 'black', className: "bold-text"};
+		cells[i][j] = {textContent: NaN, backgroundShade: '', color: 'black', className: "bold-text"}
 		// Non-index cells use a function to calculate column modulo row, have a shade propery based on the absolute value, and use two colors for contrasting shades.
-		} else {
+		} else if (j {
 		const modulo = adjustedJ - (adjustedI * Math.floor(adjustedJ / adjustedI));
 		const natural = Math.abs(modulo);
 		cells[i][j] = {textContent: modulo, backgroundShade: shadeMap[natural], color: (natural > limit / 2 ? 'darkblue' : 'lightblue')};
